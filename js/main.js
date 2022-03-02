48a069d02d105ab47e0c335c82f12e97730ca4a2
Nick Doyle <hello@nickdoyle.com>
2022-03-01 22:20:55 -0500

    Module 11 - Todolist

--- js/main.js
+++ js/main.js
@@ -1538,7 +1538,6 @@ testClearKey()
 tests.forEach(runTest)
 
 // Popover
-
 const popoverTriggers = document.querySelectorAll('.popover-trigger')
 // ========================
 // Functions
@@ -1693,4 +1692,74 @@ textArea.addEventListener('focus', evt => {
 })
 textArea.addEventListener('blur', evt => {
   console.log(evt.target.value)
+})
+
+// To Do List
+
+// ========================
+// Functions
+// ========================
+const makeTaskElement = taskname => {
+  const taskElement = document.createElement('li')
+  taskElement.classList.add('task')
+  const uniqueID = generateUniqueString(10)
+  taskElement.innerHTML = DOMPurify.sanitize(`
+    <input type="checkbox" id="${uniqueID}" />
+    <label for="${uniqueID}">
+      <svg viewBox="0 0 20 15">
+        <path d="M0 8l2-2 5 5L18 0l2 2L7 15z" fill-rule="nonzero" />
+      </svg>
+    </label>
+    <span class="task__name">${taskname}</span>
+    <button type="button" class="task__delete-button">
+      <svg viewBox="0 0 20 20">
+        <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+      </svg>
+    </button>`)
+  return taskElement
+}
+
+// ========================
+// Execution
+// ========================
+
+const todolist = document.querySelector('.todolist')
+
+todolist.addEventListener('submit', evt => {
+  evt.preventDefault()
+
+  // Add a new task
+  // 1. Get what the user typed
+  const newTaskField = todolist.querySelector('#new-task')
+  const inputValue = newTaskField.value.trim()
+  // console.log(inputValue)
+  if (!inputValue) return // prevent adding empty task
+
+  // 2. Create the task element
+  const taskElement = makeTaskElement(inputValue)
+
+  // 3. Add the task element into the DOM
+  const taskList = todolist.querySelector('.todolist__tasks')
+  todolist.addEventListener('submit', evt => {
+    taskList.appendChild(taskElement)
+  })
+
+  // 4. After submission
+  newTaskField.value = '' // clear the new task field
+  newTaskField.focus() // bring focus back to input field after entry
+
+  // 5. Delete a task
+  todolist.addEventListener('click', evt => {
+    if (!evt.target.matches('.task__delete-button')) { return }
+
+    const taskElement = evt.target.parentElement
+
+    // Removes the task
+    taskList.removeChild(taskElement)
+
+    // Triggers empty state
+    if (taskList.children.length === 0) {
+      taskList.innerHTML = ''
+    }
+  })
 })
