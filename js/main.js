bec165796a9975456b551189663f833fb4888f10
Nick Doyle <hello@nickdoyle.com>
2022-03-03 21:53:11 -0500

    Module 11 - Typeahead

--- js/main.js
+++ js/main.js
@@ -1763,3 +1763,341 @@ todolist.addEventListener('submit', evt => {
     }
   })
 })
+
+// Typeahead
+
+// ========================
+// Variables
+// ========================
+
+// List of countries.
+// Scroll past this list to code.
+const countries = [
+  { name: 'Afghanistan' },
+  { name: 'Aland Islands' },
+  { name: 'Albania' },
+  { name: 'Algeria' },
+  { name: 'American Samoa' },
+  { name: 'Andorra' },
+  { name: 'Angola' },
+  { name: 'Anguilla' },
+  { name: 'Antarctica' },
+  { name: 'Antigua and Barbuda' },
+  { name: 'Argentina' },
+  { name: 'Armenia' },
+  { name: 'Aruba' },
+  { name: 'Australia' },
+  { name: 'Austria' },
+  { name: 'Azerbaijan' },
+  { name: 'Bahamas' },
+  { name: 'Bahrain' },
+  { name: 'Bangladesh' },
+  { name: 'Barbados' },
+  { name: 'Belarus' },
+  { name: 'Belgium' },
+  { name: 'Belize' },
+  { name: 'Benin' },
+  { name: 'Bermuda' },
+  { name: 'Bhutan' },
+  { name: 'Bolivia' },
+  { name: 'Bonaire, Sint Eustatius and Saba' },
+  { name: 'Bosnia and Herzegovina' },
+  { name: 'Botswana' },
+  { name: 'Bouvet Island' },
+  { name: 'Brazil' },
+  { name: 'British Indian Ocean Territory' },
+  { name: 'United States Minor Outlying Islands' },
+  { name: 'Virgin Islands (British)' },
+  { name: 'Virgin Islands (U.S.)' },
+  { name: 'Brunei Darussalam' },
+  { name: 'Bulgaria' },
+  { name: 'Burkina Faso' },
+  { name: 'Burundi' },
+  { name: 'Cambodia' },
+  { name: 'Cameroon' },
+  { name: 'Canada' },
+  { name: 'Cabo Verde' },
+  { name: 'Cayman Islands' },
+  { name: 'Central African Republic' },
+  { name: 'Chad' },
+  { name: 'Chile' },
+  { name: 'China' },
+  { name: 'Christmas Island' },
+  { name: 'Cocos (Keeling) Islands' },
+  { name: 'Colombia' },
+  { name: 'Comoros' },
+  { name: 'Congo' },
+  { name: 'Congo (Democratic Republic of the)' },
+  { name: 'Cook Islands' },
+  { name: 'Costa Rica' },
+  { name: 'Croatia' },
+  { name: 'Cuba' },
+  { name: 'Curaçao' },
+  { name: 'Cyprus' },
+  { name: 'Czech Republic' },
+  { name: 'Denmark' },
+  { name: 'Djibouti' },
+  { name: 'Dominica' },
+  { name: 'Dominican Republic' },
+  { name: 'Ecuador' },
+  { name: 'Egypt' },
+  { name: 'El Salvador' },
+  { name: 'Equatorial Guinea' },
+  { name: 'Eritrea' },
+  { name: 'Estonia' },
+  { name: 'Ethiopia' },
+  { name: 'Falkland Islands (Malvinas)' },
+  { name: 'Faroe Islands' },
+  { name: 'Fiji' },
+  { name: 'Finland' },
+  { name: 'France' },
+  { name: 'French Guiana' },
+  { name: 'French Polynesia' },
+  { name: 'French Southern Territories' },
+  { name: 'Gabon' },
+  { name: 'Gambia' },
+  { name: 'Georgia' },
+  { name: 'Germany' },
+  { name: 'Ghana' },
+  { name: 'Gibraltar' },
+  { name: 'Greece' },
+  { name: 'Greenland' },
+  { name: 'Grenada' },
+  { name: 'Guadeloupe' },
+  { name: 'Guam' },
+  { name: 'Guatemala' },
+  { name: 'Guernsey' },
+  { name: 'Guinea' },
+  { name: 'Guinea-Bissau' },
+  { name: 'Guyana' },
+  { name: 'Haiti' },
+  { name: 'Heard Island and McDonald Islands' },
+  { name: 'Holy See' },
+  { name: 'Honduras' },
+  { name: 'Hong Kong' },
+  { name: 'Hungary' },
+  { name: 'Iceland' },
+  { name: 'India' },
+  { name: 'Indonesia' },
+  { name: "Côte d'Ivoire" },
+  { name: 'Iran (Islamic Republic of)' },
+  { name: 'Iraq' },
+  { name: 'Ireland' },
+  { name: 'Isle of Man' },
+  { name: 'Israel' },
+  { name: 'Italy' },
+  { name: 'Jamaica' },
+  { name: 'Japan' },
+  { name: 'Jersey' },
+  { name: 'Jordan' },
+  { name: 'Kazakhstan' },
+  { name: 'Kenya' },
+  { name: 'Kiribati' },
+  { name: 'Kuwait' },
+  { name: 'Kyrgyzstan' },
+  { name: "Lao People's Democratic Republic" },
+  { name: 'Latvia' },
+  { name: 'Lebanon' },
+  { name: 'Lesotho' },
+  { name: 'Liberia' },
+  { name: 'Libya' },
+  { name: 'Liechtenstein' },
+  { name: 'Lithuania' },
+  { name: 'Luxembourg' },
+  { name: 'Macao' },
+  { name: 'Macedonia (the former Yugoslav Republic of)' },
+  { name: 'Madagascar' },
+  { name: 'Malawi' },
+  { name: 'Malaysia' },
+  { name: 'Maldives' },
+  { name: 'Mali' },
+  { name: 'Malta' },
+  { name: 'Marshall Islands' },
+  { name: 'Martinique' },
+  { name: 'Mauritania' },
+  { name: 'Mauritius' },
+  { name: 'Mayotte' },
+  { name: 'Mexico' },
+  { name: 'Micronesia (Federated States of)' },
+  { name: 'Moldova (Republic of)' },
+  { name: 'Monaco' },
+  { name: 'Mongolia' },
+  { name: 'Montenegro' },
+  { name: 'Montserrat' },
+  { name: 'Morocco' },
+  { name: 'Mozambique' },
+  { name: 'Myanmar' },
+  { name: 'Namibia' },
+  { name: 'Nauru' },
+  { name: 'Nepal' },
+  { name: 'Netherlands' },
+  { name: 'New Caledonia' },
+  { name: 'New Zealand' },
+  { name: 'Nicaragua' },
+  { name: 'Niger' },
+  { name: 'Nigeria' },
+  { name: 'Niue' },
+  { name: 'Norfolk Island' },
+  { name: 'North Korea' },
+  { name: 'Northern Mariana Islands' },
+  { name: 'Norway' },
+  { name: 'Oman' },
+  { name: 'Pakistan' },
+  { name: 'Palau' },
+  { name: 'Palestine, State of' },
+  { name: 'Panama' },
+  { name: 'Papua New Guinea' },
+  { name: 'Paraguay' },
+  { name: 'Peru' },
+  { name: 'Philippines' },
+  { name: 'Pitcairn' },
+  { name: 'Poland' },
+  { name: 'Portugal' },
+  { name: 'Puerto Rico' },
+  { name: 'Qatar' },
+  { name: 'Republic of Kosovo' },
+  { name: 'Réunion' },
+  { name: 'Romania' },
+  { name: 'Russian Federation' },
+  { name: 'Rwanda' },
+  { name: 'Saint Barthélemy' },
+  { name: 'Saint Helena, Ascension and Tristan da Cunha' },
+  { name: 'Saint Kitts and Nevis' },
+  { name: 'Saint Lucia' },
+  { name: 'Saint Martin (French part)' },
+  { name: 'Saint Pierre and Miquelon' },
+  { name: 'Saint Vincent and the Grenadines' },
+  { name: 'Samoa' },
+  { name: 'San Marino' },
+  { name: 'Sao Tome and Principe' },
+  { name: 'Saudi Arabia' },
+  { name: 'Senegal' },
+  { name: 'Serbia' },
+  { name: 'Seychelles' },
+  { name: 'Sierra Leone' },
+  { name: 'Singapore' },
+  { name: 'Sint Maarten' },
+  { name: 'Slovakia' },
+  { name: 'Slovenia' },
+  { name: 'Solomon Islands' },
+  { name: 'Somalia' },
+  { name: 'South Africa' },
+  { name: 'South Georgia and the South Sandwich Islands' },
+  { name: 'South Korea' },
+  { name: 'South Sudan' },
+  { name: 'Spain' },
+  { name: 'Sri Lanka' },
+  { name: 'Sudan' },
+  { name: 'Suriname' },
+  { name: 'Svalbard and Jan Mayen' },
+  { name: 'Swaziland' },
+  { name: 'Sweden' },
+  { name: 'Switzerland' },
+  { name: 'Syrian Arab Republic' },
+  { name: 'Taiwan' },
+  { name: 'Tajikistan' },
+  { name: 'Tanzania, United Republic of' },
+  { name: 'Thailand' },
+  { name: 'Timor-Leste' },
+  { name: 'Togo' },
+  { name: 'Tokelau' },
+  { name: 'Tonga' },
+  { name: 'Trinidad and Tobago' },
+  { name: 'Tunisia' },
+  { name: 'Turkey' },
+  { name: 'Turkmenistan' },
+  { name: 'Turks and Caicos Islands' },
+  { name: 'Tuvalu' },
+  { name: 'Uganda' },
+  { name: 'Ukraine' },
+  { name: 'United Arab Emirates' },
+  { name: 'United Kingdom' },
+  { name: 'United States' },
+  { name: 'Uruguay' },
+  { name: 'Uzbekistan' },
+  { name: 'Vanuatu' },
+  { name: 'Venezuela' },
+  { name: 'Vietnam' },
+  { name: 'Wallis and Futuna' },
+  { name: 'Western Sahara' },
+  { name: 'Yemen' },
+  { name: 'Zambia' },
+  { name: 'Zimbabwe' }
+]
+
+// ========================
+// Functions
+// ========================
+
+// Showing a list of predictions
+// 1. Find out what the user typed
+const typeahead = document.querySelector('.typeahead')
+const input = typeahead.querySelector('input')
+const ul = typeahead.querySelector('ul')
+
+input.addEventListener('input', evt => {
+  // Get what the user typed
+  const input = evt.target
+  const inputValue = input.value.trim().toLowerCase()
+  // console.log(inputValue)
+
+  // Hides list if user typed nothing
+  // (or empties input field)
+  if (!inputValue) return ul.setAttribute('hidden', true)
+
+  // 2. Filter countries according to what they typed
+  const matches = countries.filter(country => {
+    const name = country.name.toLowerCase()
+    return name.startsWith(inputValue)
+  })
+  // console.log(matches)
+
+  // 3. Make HTML elements for each prediction
+  // Create an array of <li> elements by running matches with a map function
+  const listItems = matches.map(country => {
+    // Bolds search terms
+    return `<li>${boldSearchTerms(country.name, inputValue)}</li>`
+    // extract the country's name from each country object (from the countries array of objects)
+  })
+    .join('') // use join to create the innerHTML for <ul>
+  // console.log(listItems)
+
+  // 4. Show the list
+  // To show the list, we set the <ul>'s innerHTML to listItems.
+  ul.innerHTML = listItems
+  ul.removeAttribute('hidden') // then display the list by removing the hidden attribute
+})
+
+// Selecting a prediction
+// If the user clicks a prediction, we want to replace the input value with the prediction.
+ul.addEventListener('click', evt => {
+  if (!evt.target.matches('li')) return // To do this, we listen for a click event on the <ul> element. (Event delegation pattern).
+  // We want to get the name of the country in the <li>. We can get the name with textContent.
+  const li = evt.target
+  const countryName = li.textContent
+  // console.log(countryName)
+
+  // Then, we replace the input field’s value with the country’s name.
+  input.value = countryName
+
+  // The list of predictions is redundant now since the user has made a choice so we’ll hide the list.
+  ul.setAttribute('hidden', true)
+})
+
+// Hiding list of predictions
+document.addEventListener('click', evt => {
+  if (!evt.target.closest('.typeahead')) {
+    ul.setAttribute('hidden', true)
+  }
+})
+
+const boldSearchTerms = (string, searchTerms) => {
+  const length = searchTerms.length
+  const toBold = string.substring(0, length)
+  const restOfString = string.substring(length)
+  return `<strong>${toBold}</strong>${restOfString}`
+}
+
+// ========================
+// Execution
+// ========================
